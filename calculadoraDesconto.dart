040e1078232aefa9e5b205867c21cdfe0e6b3f21
Jose Teixeira <texrex9@gmail.com>
2026-04-28 11:17:39 -0300

    correcao codigo

--- calculadoraDesconto.dart
+++ calculadoraDesconto.dart
@@ -1,6 +1,7 @@
 void main() {
   double valorDoProduto = 150.0;
   bool temCupomDesconto = true;
+  // ignore: dead_code
   double valorFinal = temCupomDesconto ? valorDoProduto * 0.85 : valorDoProduto;
 
   print("Valor final do produto: R\$ ${valorFinal.toStringAsFixed(2)}");
--- validadorAcesso.dart
+++ validadorAcesso.dart
@@ -1,8 +1,7 @@
 void main() {
   String? nomeUsuario;
   String exibicao = "Visitante";
-
+  // ignore: unnecessary_null_comparison
   nomeUsuario == null ? exibicao = "Visitante" : exibicao = nomeUsuario;
-
   print("Bem vindo, $exibicao!");
 }
