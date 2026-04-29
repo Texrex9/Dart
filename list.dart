a887e0f6eb409a6a9a792b97ffdf43fa9ac2b5f8
Jose Teixeira <texrex9@gmail.com>
2026-04-29 11:34:16 -0300

    criação de list

---
+++ list.dart
@@ -0,0 +1,21 @@
+void main() {
+  List minhaListaGenerica1 = [1, "João", true];
+
+  List minhaListaGenerica2 = [];
+
+  List minhaListaGenerica3 = List.empty();
+
+  List? minhaListaNullable;
+
+  print("Print após criado da lista 1: $minhaListaGenerica1");
+
+  // Adicionando novos elementos
+
+  minhaListaGenerica1.add("Azul");
+  print("Print após adição de um novo elemento: $minhaListaGenerica1");
+
+  // Removendo elemento
+
+  minhaListaGenerica1.removeAt(2);
+  print("Print após remoção de um elemento: $minhaListaGenerica1");
+}
